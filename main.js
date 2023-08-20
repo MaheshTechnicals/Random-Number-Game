64c118d700b4b90cb9de4f429281419e829a25a5
msvarma10001@gmail.com <msvarma10001@gmail.com>
2023-08-20 09:24:12 +0530

    Animate element

--- main.js
+++ main.js
@@ -1,3 +1,34 @@
 let g1 = document.querySelector("#one")
 let g2 = document.querySelector("#two")
 let g3 = document.querySelector("#three")
+
+let btn = document.querySelector("#btn")
+
+
+btn.addEventListener("click", () => {
+
+  animateGlass()
+
+})
+
+
+
+
+
+
+
+
+
+let animateGlass = () => {
+  gsap.to(g1, {
+    x: 240,
+    duration: 0.3,
+    repeat: 5,
+  })
+
+  gsap.to(g3, {
+    x: -240,
+    duration: 0.3,
+    repeat: 5,
+  })
+}
